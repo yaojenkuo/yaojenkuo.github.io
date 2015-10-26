a73198f0f8c9d9d6cb34f5c8387e5a643efc47cc
Yao-Jen <Tony.Kuo@sas.com>
2015-10-26 15:58:56 +0800

    10262015 Commit

--- js/stylishPortfolio.js
+++ js/stylishPortfolio.js
@@ -26,3 +26,16 @@ $(function() {
         }
     });
 });
+
+var map = L.map('map').setView([25.059, 121.541], 15);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoidG9ueWt1b3lqIiwiYSI6ImNpZnJ2cjV1bjB4YzNzMm0wcXE4a2ZhaTIifQ.ZH43M-eU67KjPAM3w__IvQ'
+}).addTo(map);
+
+var marker = L.marker([25.058, 121.539]).addTo(map);
+
+marker.bindPopup("<b>SAS Institute, TW</b>");
