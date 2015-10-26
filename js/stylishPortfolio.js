04235433a44f2041c63d955cf0a8c0233c9b7238
Yao-Jen <Tony.Kuo@sas.com>
2015-10-26 16:07:26 +0800

    10262015 Commit

--- js/stylishPortfolio.js
+++ js/stylishPortfolio.js
@@ -28,7 +28,7 @@ $(function() {
 });
 
 //Leaflet map
-var map = L.map('map').setView([25.059, 121.541], 15);
+var map = L.map('map').setView([25.059, 121.541], 14);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
