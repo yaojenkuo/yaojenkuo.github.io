2664fffe02bbbb6afd0fa39fb713a4c9e813f7e4
Yao-Jen <tonykuoyj@gmail.com>
2016-04-11 18:38:44 +0800

    added contents

--- fsiForum2016/js/plugins/morris/morris-data.js
+++ fsiForum2016/js/plugins/morris/morris-data.js
@@ -194,7 +194,69 @@ $(function() {
 
     // Bar Chart
     Morris.Bar({
-        element: 'morris-bar-chart',
+        element: 'morris-bar-chart-1',
+        data: [{
+            device: 'iPhone',
+            geekbench: 136
+        }, {
+            device: 'iPhone 3G',
+            geekbench: 137
+        }, {
+            device: 'iPhone 3GS',
+            geekbench: 275
+        }, {
+            device: 'iPhone 4',
+            geekbench: 380
+        }, {
+            device: 'iPhone 4S',
+            geekbench: 655
+        }, {
+            device: 'iPhone 5',
+            geekbench: 1571
+        }],
+        xkey: 'device',
+        ykeys: ['geekbench'],
+        labels: ['Geekbench'],
+        barRatio: 0.4,
+        xLabelAngle: 35,
+        hideHover: 'auto',
+        resize: true
+    });
+
+    // Bar Chart
+    Morris.Bar({
+        element: 'morris-bar-chart-2',
+        data: [{
+            device: 'iPhone',
+            geekbench: 136
+        }, {
+            device: 'iPhone 3G',
+            geekbench: 137
+        }, {
+            device: 'iPhone 3GS',
+            geekbench: 275
+        }, {
+            device: 'iPhone 4',
+            geekbench: 380
+        }, {
+            device: 'iPhone 4S',
+            geekbench: 655
+        }, {
+            device: 'iPhone 5',
+            geekbench: 1571
+        }],
+        xkey: 'device',
+        ykeys: ['geekbench'],
+        labels: ['Geekbench'],
+        barRatio: 0.4,
+        xLabelAngle: 35,
+        hideHover: 'auto',
+        resize: true
+    });
+
+    // Bar Chart
+    Morris.Bar({
+        element: 'morris-bar-chart-3',
         data: [{
             device: 'iPhone',
             geekbench: 136
