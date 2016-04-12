feac29748450e4870d4fc57dbc3b3b914f8d5658
Yao-Jen <tonykuoyj@gmail.com>
2016-04-12 10:00:30 +0800

    revised contents

--- fsiForum2016/js/plugins/googleCharts.js
+++ fsiForum2016/js/plugins/googleCharts.js
@@ -1,5 +1,7 @@
 google.charts.load("current", { packages: ["corechart"] });
 google.charts.setOnLoadCallback(drawChart1);
+google.charts.setOnLoadCallback(drawChart2);
+google.charts.setOnLoadCallback(drawChart3);
 
 function drawChart1() {
     var data = google.visualization.arrayToDataTable([
@@ -13,10 +15,50 @@ function drawChart1() {
     ]);
 
     var options = {
-        title: 'My Daily Activities',
+        title: '資產配置',
         pieHole: 0.4,
     };
 
     var chart = new google.visualization.PieChart(document.getElementById('donutchart1'));
+    chart.draw(data, options);
+}
+
+function drawChart2() {
+    var data = google.visualization.arrayToDataTable([
+        ['Fund', 'Dollar'],
+        ['全球股票型基金', 11],
+        ['區域股票型基金', 2],
+        ['新興股票型基金', 3],
+        ['一般債券型基金', 2],
+        ['高收益債券型基金', 7],
+        ['主題債券型基金', 5]
+    ]);
+
+    var options = {
+        title: '資產配置',
+        pieHole: 0.4,
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById('donutchart2'));
+    chart.draw(data, options);
+}
+
+function drawChart3() {
+    var data = google.visualization.arrayToDataTable([
+        ['Fund', 'Dollar'],
+        ['全球股票型基金', 11],
+        ['區域股票型基金', 2],
+        ['新興股票型基金', 3],
+        ['一般債券型基金', 2],
+        ['高收益債券型基金', 7],
+        ['主題債券型基金', 5]
+    ]);
+
+    var options = {
+        title: '資產配置',
+        pieHole: 0.4,
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById('donutchart3'));
     chart.draw(data, options);
 }
