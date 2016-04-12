a1302b05629b10032bdbfea944b4e89a42b8cc37
Yao-Jen <tonykuoyj@gmail.com>
2016-04-12 10:49:13 +0800

    revised contents

---
+++ fsiForum2016/js/plugins/googleCharts1.js
@@ -0,0 +1,22 @@
+google.charts.load("current", { packages: ["corechart"] });
+google.charts.setOnLoadCallback(drawChart1);
+
+function drawChart1() {
+    var data = google.visualization.arrayToDataTable([
+        ['Fund', 'Dollar'],
+        ['全球股票型基金', 11],
+        ['區域股票型基金', 2],
+        ['新興股票型基金', 3],
+        ['一般債券型基金', 2],
+        ['高收益債券型基金', 7],
+        ['主題債券型基金', 5]
+    ]);
+
+    var options = {
+        title: '資產配置',
+        pieHole: 0.4,
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById('donutchart1'));
+    chart.draw(data, options);
+}
