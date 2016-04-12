36cd3f1d739840311b6d786be051e4c2b9fc1260
Yao-Jen <tonykuoyj@gmail.com>
2016-04-12 15:43:06 +0800

    revised contents

--- sbAdmin/js/plugins/googleCharts.js
+++ sbAdmin/js/plugins/googleCharts.js
@@ -1,80 +1,47 @@
-google.charts.load("current", { packages: ["corechart"] });
+google.charts.load('current', { 'packages': ['gauge'] });
 google.charts.setOnLoadCallback(drawChart1);
 google.charts.setOnLoadCallback(drawChart2);
-google.charts.setOnLoadCallback(drawChart3);
-/*
-$('a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
-    if ($(e.target).attr('id') == 'menu1') {
-        drawChart2();
-    }
-    if ($(e.target).attr('id') == 'menu2') {
-        drawChart3();
-    }
-});*/
 
 function drawChart1() {
+
     var data = google.visualization.arrayToDataTable([
-        ['Fund', 'Dollar'],
-        ['全球股票型基金', 11],
-        ['區域股票型基金', 2],
-        ['新興股票型基金', 3],
-        ['一般債券型基金', 2],
-        ['高收益債券型基金', 7],
-        ['主題債券型基金', 5]
+        ['Label', 'Value'],
+        ['自選', 80]
     ]);
 
     var options = {
-        title: '資產配置',
-        pieHole: 0.4,
+        width: 600,
+        height: 480,
+        redFrom: 90,
+        redTo: 100,
+        yellowFrom: 75,
+        yellowTo: 90,
+        minorTicks: 5
     };
 
-    var chart = new google.visualization.PieChart(document.getElementById('donutchart1'));
-    chart.draw(data, options);
+    var chart = new google.visualization.Gauge(document.getElementById('gauge1'));
 
+    chart.draw(data, options);
 }
 
 function drawChart2() {
+
     var data = google.visualization.arrayToDataTable([
-        ['Fund', 'Dollar'],
-        ['全球股票型基金', 11],
-        ['區域股票型基金', 2],
-        ['新興股票型基金', 3],
-        ['一般債券型基金', 2],
-        ['高收益債券型基金', 7],
-        ['主題債券型基金', 5]
+        ['Label', 'Value'],
+        ['推薦', 90]
     ]);
 
     var options = {
-        title: '資產配置',
-        pieHole: 0.4,
-        width: 900,
-        height: 500
+        width: 600,
+        height: 480,
+        redFrom: 90,
+        redTo: 100,
+        yellowFrom: 75,
+        yellowTo: 90,
+        minorTicks: 5
     };
 
-    var chart = new google.visualization.PieChart(document.getElementById('donutchart2'));
+    var chart = new google.visualization.Gauge(document.getElementById('gauge2'));
+
     chart.draw(data, options);
-
-}
-
-function drawChart3() {
-    var data = google.visualization.arrayToDataTable([
-        ['Fund', 'Dollar'],
-        ['全球股票型基金', 11],
-        ['區域股票型基金', 2],
-        ['新興股票型基金', 3],
-        ['一般債券型基金', 2],
-        ['高收益債券型基金', 7],
-        ['主題債券型基金', 5]
-    ]);
-
-    var options = {
-        title: '資產配置',
-        pieHole: 0.4,
-        width: 900,
-        height: 500
-    };
-
-    var chart = new google.visualization.PieChart(document.getElementById('donutchart3'));
-    chart.draw(data, options);
-
 }
