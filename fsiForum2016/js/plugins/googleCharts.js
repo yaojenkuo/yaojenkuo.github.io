ab2226d5af278f2070f486c6fe4fb5689d08f1d8
Yao-Jen <tonykuoyj@gmail.com>
2016-04-14 16:21:13 +0800

    revised contents

--- fsiForum2016/js/plugins/googleCharts.js
+++ fsiForum2016/js/plugins/googleCharts.js
@@ -1,20 +1,10 @@
 google.charts.load('current', { packages: ['geochart', 'corechart'] });
 google.charts.setOnLoadCallback(drawRegionsMap);
 google.charts.setOnLoadCallback(drawAreaChart);
-//google.charts.setOnLoadCallback(drawScatterChart1);
+google.charts.setOnLoadCallback(drawBubbleChart1);
 google.charts.setOnLoadCallback(drawDonutChart);
-//google.charts.setOnLoadCallback(drawScatterChart2);
-//google.charts.setOnLoadCallback(drawChart2);
-//google.charts.setOnLoadCallback(drawChart3);
-/*
-$('a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
-    if ($(e.target).attr('id') == 'menu1') {
-        drawChart2();
-    }
-    if ($(e.target).attr('id') == 'menu2') {
-        drawChart3();
-    }
-});*/
+google.charts.setOnLoadCallback(drawBubbleChart2);
+
 
 function drawDonutChart() {
     var data = google.visualization.arrayToDataTable([
@@ -29,9 +19,7 @@ function drawDonutChart() {
 
     var options = {
         title: '資產配置',
-        pieHole: 0.4,
-        width: 900,
-        height: 500
+        pieHole: 0.5
     };
 
     var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
@@ -83,200 +71,42 @@ function drawAreaChart() {
     chart.draw(data, options);
 }
 
-function drawScatterChart1() {
-
-    var data = new google.visualization.DataTable();
-    data.addColumn('number', 'Hours Studied');
-    data.addColumn('number', 'Final');
-
-    data.addRows([
-        [0, 67],
-        [1, 88],
-        [2, 77],
-        [3, 93],
-        [4, 85],
-        [5, 91],
-        [6, 71],
-        [7, 78],
-        [8, 93],
-        [9, 80],
-        [10, 82],
-        [0, 75],
-        [5, 80],
-        [3, 90],
-        [1, 72],
-        [5, 75],
-        [6, 68],
-        [7, 98],
-        [3, 82],
-        [9, 94],
-        [2, 79],
-        [2, 95],
-        [2, 86],
-        [3, 67],
-        [4, 60],
-        [2, 80],
-        [6, 92],
-        [2, 81],
-        [8, 79],
-        [9, 83],
-        [3, 75],
-        [1, 80],
-        [3, 71],
-        [3, 89],
-        [4, 92],
-        [5, 85],
-        [6, 92],
-        [7, 78],
-        [6, 95],
-        [3, 81],
-        [0, 64],
-        [4, 85],
-        [2, 83],
-        [3, 96],
-        [4, 77],
-        [5, 89],
-        [4, 89],
-        [7, 84],
-        [4, 92],
-        [9, 98]
-    ]);
-
-    var options = {
-        width: 800,
-        height: 500,
-        chart: {
-            title: 'Students\' Final Grades',
-            subtitle: 'based on hours studied'
-        },
-        hAxis: { title: 'Hours Studied' },
-        vAxis: { title: 'Grade' }
-    };
-
-    var chart = new google.charts.Scatter(document.getElementById('efficientFrontier'));
-
-    chart.draw(data, options);
-}
-
-function drawScatterChart2() {
-
-    var data = new google.visualization.DataTable();
-    data.addColumn('number', 'Hours Studied');
-    data.addColumn('number', 'Final');
-
-    data.addRows([
-        [0, 67],
-        [1, 88],
-        [2, 77],
-        [3, 93],
-        [4, 85],
-        [5, 91],
-        [6, 71],
-        [7, 78],
-        [8, 93],
-        [9, 80],
-        [10, 82],
-        [0, 75],
-        [5, 80],
-        [3, 90],
-        [1, 72],
-        [5, 75],
-        [6, 68],
-        [7, 98],
-        [3, 82],
-        [9, 94],
-        [2, 79],
-        [2, 95],
-        [2, 86],
-        [3, 67],
-        [4, 60],
-        [2, 80],
-        [6, 92],
-        [2, 81],
-        [8, 79],
-        [9, 83],
-        [3, 75],
-        [1, 80],
-        [3, 71],
-        [3, 89],
-        [4, 92],
-        [5, 85],
-        [6, 92],
-        [7, 78],
-        [6, 95],
-        [3, 81],
-        [0, 64],
-        [4, 85],
-        [2, 83],
-        [3, 96],
-        [4, 77],
-        [5, 89],
-        [4, 89],
-        [7, 84],
-        [4, 92],
-        [9, 98]
-    ]);
-
-    var options = {
-        width: 800,
-        height: 500,
-        chart: {
-            title: 'Students\' Final Grades',
-            subtitle: 'based on hours studied'
-        },
-        hAxis: { title: 'Hours Studied' },
-        vAxis: { title: 'Grade' }
-    };
-
-    var chart = new google.charts.Scatter(document.getElementById('scatterplot'));
-
-    chart.draw(data, options);
-}
-
-/*
-function drawChart2() {
+function drawBubbleChart1() {
     var data = google.visualization.arrayToDataTable([
-        ['Fund', 'Dollar'],
-        ['全球股票型基金', 11],
-        ['區域股票型基金', 2],
-        ['新興股票型基金', 3],
-        ['一般債券型基金', 2],
-        ['高收益債券型基金', 7],
-        ['主題債券型基金', 5]
+        ['ID', 'X', 'Y', 'Temperature'],
+        ['', 80, 167, 120],
+        ['', 79, 136, 130],
+        ['', 78, 184, 50],
+        ['', 72, 278, 230],
+        ['', 81, 200, 210],
+        ['', 72, 170, 100],
+        ['', 68, 477, 80]
     ]);
 
     var options = {
-        title: '資產配置',
-        pieHole: 0.4,
-        width: 900,
-        height: 500
+        colorAxis: { colors: ['yellow', 'red'] }
     };
 
-    var chart = new google.visualization.PieChart(document.getElementById('donutchart2'));
+    var chart = new google.visualization.BubbleChart(document.getElementById('bubble1'));
     chart.draw(data, options);
-
 }
 
-function drawChart3() {
+function drawBubbleChart2() {
     var data = google.visualization.arrayToDataTable([
-        ['Fund', 'Dollar'],
-        ['全球股票型基金', 11],
-        ['區域股票型基金', 2],
-        ['新興股票型基金', 3],
-        ['一般債券型基金', 2],
-        ['高收益債券型基金', 7],
-        ['主題債券型基金', 5]
+        ['ID', 'X', 'Y', 'Temperature'],
+        ['', 80, 167, 120],
+        ['', 79, 136, 130],
+        ['', 78, 184, 50],
+        ['', 72, 278, 230],
+        ['', 81, 200, 210],
+        ['', 72, 170, 100],
+        ['', 68, 477, 80]
     ]);
 
     var options = {
-        title: '資產配置',
-        pieHole: 0.4,
-        width: 900,
-        height: 500
+        colorAxis: { colors: ['yellow', 'red'] }
     };
 
-    var chart = new google.visualization.PieChart(document.getElementById('donutchart3'));
+    var chart = new google.visualization.BubbleChart(document.getElementById('bubble2'));
     chart.draw(data, options);
-
 }
-*/
